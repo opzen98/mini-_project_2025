aa2a648c58b3b6084f04df9ba8366f8f06014132
Shahrin Zen <shahrinzen@tmrnd.com.my>
2025-07-09 23:14:14 +0800

    added mem0 capabilties to Agent

---
+++ Agent/configuration.py
@@ -0,0 +1,33 @@
+
+
+import os
+from dataclasses import dataclass, field, fields
+from typing import Any, Optional
+
+from langchain_core.runnables import RunnableConfig
+from typing_extensions import Annotated
+from dataclasses import dataclass
+
+@dataclass(kw_only=True)
+class Configuration:
+    """The configuration for the agent."""
+    """The configurable fields for the chatbot."""
+    user_id: str = "default-user"
+    asst_role: str ="You are an intelligent assistant equipped with a suite of tools, including code execution capabilities."
+
+    @classmethod
+    def from_runnable_config(
+        cls, config: Optional[RunnableConfig] = None
+    ) -> "Configuration":
+        """Create a Configuration instance from a RunnableConfig."""
+        configurable = (
+            config["configurable"] if config and "configurable" in config else {}
+        )
+        values: dict[str, Any] = {
+            f.name: os.environ.get(f.name.upper(), configurable.get(f.name))
+            for f in fields(cls)
+            if f.init
+        }
+        return cls(**{k: v for k, v in values.items() if v})
+
+
